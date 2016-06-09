2b6cd823eff199aecd6ad327aa5517f6e8160b17
Gregory Komar <xalvevs@gmail.com>
2016-06-09 16:01:00 +0300

    Fixes

--- ADS/bst/src/main/java/io/github/atommed/bst/BSTree.java
+++ ADS/bst/src/main/java/io/github/atommed/bst/BSTree.java
@@ -123,9 +123,19 @@ public class BSTree<K extends Comparable<K>,V> {
         successor.left.parent = successor;
         successor.right.parent = successor;
 
-        //successor.parent = node.parent;
-
         dangerReplaceNode(node,successor);
+    }
+
+    public void removeANode(Node<K,V> node){
+      if(node == head)
+      if(node.left!=null && node.right != null)
+	removeChildfullNode(node);
+      else if(node.left == null && node.right == null)
+	dangerReplaceNode(node, null);
+      else if(node.left !=null)
+	dangerReplaceNode(node, node.left);
+      else if(node.right != null)
+	dangerReplaceNode(node, node.right);
     }
 
     private void removeSimple(Node<K,V> node){
@@ -154,7 +164,7 @@ public class BSTree<K extends Comparable<K>,V> {
     }
 
     private void removeNode(Node<K,V> node){
-        if(node.parent == null) {
+        if(node == head) {
             head = null;
             return;
         }
@@ -168,12 +178,91 @@ public class BSTree<K extends Comparable<K>,V> {
             successor.right = node.right;
             node.right.parent = successor;
             successor.parent = node.parent;
+
             if(node.parent.left == node)
                 node.parent.left = successor;
             else if(node.parent.right == node)
                 node.parent.right = successor;
         }
         else removeSimple(node);
+    }
+
+    private void pReplaceSubtree(Node<K,V> u, Node<K,V> v){
+      if(u == head) head = v;
+      else {
+	Node<K,V> par = u.parent;
+	if(par.left == u){
+	  par.left = v;
+	}
+	else if(par.right == u){
+	  par.right = v;
+	}
+	if(v!=null)
+	  v.parent = par;
+      }
+    }
+
+    private void Remove(Node<K,V> node){
+      if(node.left == null)
+	pReplaceSubtree(node, node.right);
+      else if(node.right == null)
+	pReplaceSubtree(node, node.left);
+      else {
+	Node<K,V> y = findSuccessor(node);
+	if(y.parent != node){
+	  pReplaceSubtree(y,y.right);
+	  y.right = node.right;
+	  node.right.parent = y;
+	}
+	pReplaceSubtree(node, y);
+	y.left = node.left;
+	node.left.parent = y;
+      }
+    }
+
+    private void pRemoveNoChild(Node<K,V> node){
+      if(node.parent.left == node)
+	node.parent.left = null;
+      else if(node.parent.right == node)
+	node.parent.right = null;
+    }
+
+    private void pRemoveSingleChild(Node<K,V> node){
+      Node<K,V> repl = node.left != null ? node.left : node.right;
+      if(node.parent.right == node){
+       node.parent.right = repl;
+      }
+      else if(node.parent.left == node){
+	node.parent.left = repl;
+      }
+      repl.parent = node.parent;
+    }
+
+    private void pRemoveChildFull(Node<K,V> node){
+      Node<K,V> succ = findSuccessor(node);
+      if(succ.right != null)
+	pRemoveSingleChild(succ);
+      else pRemoveNoChild(succ);
+
+
+
+      succ.left = node.left;
+      if(succ.left != null)
+  	succ.left.parent = succ;
+      succ.right = node.right;
+      if(succ.right!= null)
+      succ.right.parent = succ;
+
+      succ.parent = node.parent;
+    }
+
+    private void pRemove(Node<K,V> node){
+      if(node == head) return;
+      if(node.left != null && node.right != null)
+	pRemoveChildFull(node);
+      else if(node.left == null && node.right == null)
+	pRemoveNoChild(node);
+      else pRemoveSingleChild(node);
     }
 
     private void findAll(Stack<Node<K,V>> s, Node<K,V> node, RemoveCriteria<K,V> predicate){
@@ -188,7 +277,8 @@ public class BSTree<K extends Comparable<K>,V> {
     public void removeAll(RemoveCriteria<K,V> predicate){
         Stack<Node<K, V>> nodesToRemove = new Stack<>();
         findAll(nodesToRemove,head,predicate);
-        for(Node<K,V> n : nodesToRemove)
-            removeNode(n);
+        for(Node<K,V> n : nodesToRemove){
+            Remove(n);
+    	}
     }
 }
