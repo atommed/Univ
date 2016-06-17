82f5b327d2b38dc087fc1e203edcc2145bc8a5af
Gregory K <xalvevs@gmail.com>
2016-06-17 21:00:46 +0300

    Sort

--- ADS/hometask/src/main/java/App.java
+++ ADS/hometask/src/main/java/App.java
@@ -1,9 +1,40 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class App {
 
     public static void main(String... args) throws InterruptedException {
-        /*
+
         TestDataProvider dp = new TestDataProvider();
-        String[] sa,sb,oo;
+
+        /*
+        int[] o_arr = dp.genIntsRandom(5000000);
+        long ta;
+        int[] std = null;
+        int[] my = null;
+        for (int i = 0; i < 5; i++) {
+            std = Arrays.copyOf(o_arr,o_arr.length);
+            Arrays.sort(std);
+            my = Arrays.copyOf(o_arr,o_arr.length);
+            Sorts.lsd_radix_sort(my);
+        }
+
+        std = Arrays.copyOf(o_arr,o_arr.length);
+        ta = System.nanoTime();
+        Arrays.sort(std);
+        System.out.println(System.nanoTime() -  ta);
+
+        my = Arrays.copyOf(o_arr,o_arr.length);
+        ta = System.nanoTime();
+        Sorts.lsd_radix_sort(my);
+        System.out.println(System.nanoTime() - ta);
+
+        assert Arrays.equals(std,my);
+        */
+
+
+        /*
+        String[] sa,oo;
         long ta;
 
         String[] orig_strs = dp.genStringRandom(10_000_000, 2);
@@ -26,18 +57,16 @@ public class App {
         Sorts.lsd_radix_sort(sa);
         System.out.println(System.nanoTime() - ta);
         assert Arrays.equals(oo,sa);
-
-        ta = System.nanoTime();
-        Sorts.insertionSort(sb);
-        System.out.println(System.nanoTime() - ta);
-        assert Arrays.equals(oo,sb);
         */
+
+
 
         /*
         TestDataProvider dm = new TestDataProvider();
         int[] arr = dm.genIntsReverseSorted(40);
         System.out.println(Arrays.toString(arr));
         */
+        /*
         HashTableLinear<String> s = new HashTableLinear<>();
         //HashTableQuad<String> s = new HashTableQuad<>();
         for(int i  = 0; i  <100; i++){
@@ -59,7 +88,7 @@ public class App {
         assert s.get(0).equals("Soska");
         assert s.remove(0).equals("Soska");
         assert s.get(0) == null;
-
+        */
         //System.out.println(s.get(42));
     }
 }
--- ADS/hometask/src/main/java/Sorts.java
+++ ADS/hometask/src/main/java/Sorts.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.stream.IntStream;
 
 
 public class Sorts {
@@ -72,6 +73,40 @@ public class Sorts {
             }
             arr[j+1] = v;
         }
+    }
+
+    private static int getDigit(int x, long pos){
+        return (int)(x/pos) % 10 + 9;
+    }
+
+    private static int[] count = new int[20];
+    private static int[] radixCountingSort(int[] a,long pos){
+        int[] b = new int[a.length];
+        Arrays.fill(count,0);
+
+        for(int i = 0; i < a.length; i++)
+            count[getDigit(a[i],pos)]++;
+        for(int i = 1; i < count.length; i++)
+            count[i]+= count[i-1];
+
+        for(int i = a.length - 1; i>= 0; i--){
+            int digit = getDigit(a[i],pos);
+            count[digit]--;
+            b[count[digit]] = a[i];
+        }
+
+        return b;
+    }
+
+    public static void lsd_radix_sort(int[] old_arr){
+        int[] arr = old_arr;
+        int max = IntStream.of(arr).map(Math::abs).max().getAsInt();
+        int digits = String.valueOf(max).length();
+        long dMax = (long)Math.pow(10,digits-1);
+        for(long d = 1; d <= dMax; d*=10){
+            arr = radixCountingSort(arr, d);
+        }
+        System.arraycopy(arr,0,old_arr,0,arr.length);
     }
 
     public static void lsd_radix_sort(String[] old_arr){
