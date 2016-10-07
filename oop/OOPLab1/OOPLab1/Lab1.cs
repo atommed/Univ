ee74492eb831d0e9dd5c883e14cb951ae9f9672b
Gregory K <xalvevs@gmail.com>
2016-10-08 02:41:26 +0300

    Lab 1

---
+++ oop/OOPLab1/OOPLab1/Lab1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+
+namespace OOPLab1
+{	
+	interface IMoneyGrabber{
+		void visit(LawfullMan lm);
+		void visit(Bandit b);
+	}
+
+	interface IMoneyGrabbable{
+		void accept(IMoneyGrabber g);
+	}
+	
+	abstract class EconomicUnit{
+		private static ulong uid = 0;
+		public String name{ get;private set;}
+
+		protected virtual decimal budget{ get; set;}
+
+		protected virtual void recieveMoney(EconomicUnit payer, decimal amount){
+			this.budget += amount;
+		}
+		protected virtual void pay(EconomicUnit recv, decimal amount){
+			if (this.budget < amount)
+				OnMoneyEnd();
+			else {
+				this.budget -= amount;
+				recv.recieveMoney (this, amount);
+			}
+		}
+
+		protected abstract void OnMoneyEnd();
+
+		public EconomicUnit(string name){
+			this.name = name + " #" + (uid++);
+		}
+	}
+
+	class Man : EconomicUnit{
+		public Man(string name) : base(name){
+			Console.WriteLine (this.name + " borns");
+		}
+
+		protected virtual void Die(){
+			Console.WriteLine (this.name + " dies");
+		}
+
+		protected override void OnMoneyEnd(){
+			Console.WriteLine (this.name + ": Looks like i have no more money");
+			Die();
+		}
+	}
+
+	class LawfullMan : Man, IMoneyGrabbable{
+		public LawfullMan(string name) : base("lawfull " + name){}
+		public LawfullMan() : this(NameGenerator.NextName()){}
+		public void accept(IMoneyGrabber g){
+			g.visit(this);
+		}
+	}
+		
+	class Bandit : Man, IMoneyGrabbable, IMoneyGrabber{
+		public Bandit(string name) : base("bandit " + name){}
+		public Bandit() : base("bandit " + NameGenerator.NextName()){}
+
+		public void accept(IMoneyGrabber g){
+			g.visit(this);
+		}
+
+		public void visit(LawfullMan l){
+		}
+		public void visit(Bandit b){
+		}
+	}
+	
+	class Lab1
+	{
+		public static void Main (string[] args)
+		{
+			MathNet.Numerics.Distributions.Normal nd = new MathNet.Numerics.Distributions.Normal (150, 30);
+			int a, b, c;
+			a = 0;b = 0; c = 0;
+			for (int i = 0; i < 10000; i++){
+				double res = nd.Sample();
+				if (res < 100)
+					a++;
+				else if (res > 200)
+					b++;
+				else
+					c++;
+			}
+			Console.WriteLine (a + " " + b + " " + c);
+		}
+	}
+}
---
+++ oop/OOPLab1/OOPLab1/NameGenerator.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace OOPLab1
+{
+	public class NameGenerator
+	{
+		#region Names
+		private static readonly string[] names = {
+			"Gregory",
+			"Mihail",
+			"Vlad",
+			"Sergei",
+			"Niko",
+			"Dimon",
+			"Tony",
+			"Petr",
+			"Vitya",
+			"Jenia",
+			"Oleg",
+			"Nazar",
+			"Anatolij",
+			"James",
+			"Ben",
+			"Andrei",
+			"Ivan",
+			"Pedro",
+			"John",
+			"Max",
+			"Kama",
+			"ObiVan",
+			"Luke",
+			"Darth",
+			"Magistr",
+			"Doge",
+			"Pepe",
+		};
+		private static readonly string[] surnames = {
+			"Faustin",
+			"Bellik",
+			"Frai",
+			"Rudenko",
+			"Montana",
+			"Bond",
+			"Pushkin",
+			"Laden",
+			"Korolev",
+			"Gagarin",
+			"VyrviGlaz",
+			"OrlinijGlaz",
+			"Ivanov",
+			"Doe",
+			"Progulkin",
+			"Pupkin",
+			"Sveklov",
+			"Arbuzov",
+			"Payne",
+			"Android",
+			"The Bullet",
+			"The Strong",
+			"The Smart",
+			"The Fearless",
+			"The Frog"
+		};
+		#endregion
+
+		private static readonly string[] namePairs;
+		private static Random rnd = new Random();
+
+		static NameGenerator ()
+		{
+			namePairs = new string[names.Length * surnames.Length];
+			int pos = 0;
+			foreach (var name in names)
+				foreach (var surname in surnames)
+					namePairs [pos++] = name + " " + surname;
+
+		}
+
+		public static string NextName(){
+			return namePairs[rnd.Next(0,namePairs.Length)];
+		}
+	}
+}
+
